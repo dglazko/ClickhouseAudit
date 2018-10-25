82f6b1d4014b4e1ab3b2b201c930901590bbd37b
isereb <ruxxay.coder@gmail.com>
2018-10-25 16:58:28 -0400

    Added insert function

--- src/main/java/ru/anarok/audit/ClickhouseConnection.java
+++ src/main/java/ru/anarok/audit/ClickhouseConnection.java
@@ -1,15 +1,16 @@
 package ru.anarok.audit;
 
 import lombok.extern.slf4j.Slf4j;
+import ru.anarok.audit.domain.AuditEvent;
+import ru.anarok.audit.utils.IdUtils;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.Map;
 
 @Slf4j
 public class ClickhouseConnection {
     private Connection connection;
+    private final IdUtils idUtils = new IdUtils();
 
     public void connect(String host) throws SQLException {
         connect(host, 8123, null, null, null);
@@ -34,7 +35,7 @@ public class ClickhouseConnection {
     protected void createTables() throws SQLException {
         executeQuery(
             "CREATE TABLE IF NOT EXISTS Audits (" +
-                "id UInt64," +
+                "auditId UInt64," +
                 "timestamp DateTime," +
                 "emitter String, " +
                 "type String," +
@@ -51,8 +52,8 @@ public class ClickhouseConnection {
                 "name String," +
                 "value String" +
                 ") ENGINE = MergeTree() " +
-                "PARTITION BY auditId " +
-                "ORDER BY auditId " +
+                "PARTITION BY (auditId, name) " +
+                "ORDER BY (auditId, name) " +
                 "SETTINGS index_granularity=8192"
         );
     }
@@ -61,6 +62,31 @@ public class ClickhouseConnection {
         return connection.prepareStatement(sql).executeQuery();
     }
 
+    public void insert(AuditEvent e) throws SQLException {
+        long id = idUtils.getRecordId();
+        PreparedStatement stmt = connection.prepareStatement(
+            "INSERT INTO Audits (auditId, timestamp, emitter, type, message)" +
+                "VALUES (?, ?, ?, ?, ?)"
+        );
+        stmt.setLong(1, id);
+        stmt.setLong(2, e.getTimestamp() / 1000);
+        stmt.setString(3, e.getEmitter());
+        stmt.setString(4, e.getType());
+        stmt.setString(5, e.getMessage());
+        stmt.executeUpdate();
+        stmt.close();
+
+        for (Map.Entry<String, String> stringStringEntry : e.getData().entrySet()) {
+            stmt = connection.prepareStatement(
+                "INSERT INTO AuditAttributes (auditId, name, value) VALUES (?,?,?)"
+            );
+            stmt.setLong(1, id);
+            stmt.setString(2, stringStringEntry.getKey());
+            stmt.setString(3, stringStringEntry.getValue());
+            stmt.executeUpdate();
+            stmt.close();
+        }
+    }
 
     public void shutdownImmediately() throws SQLException {
         connection.close();
