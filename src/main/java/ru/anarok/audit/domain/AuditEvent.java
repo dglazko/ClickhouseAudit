722d22938ad5d9480d410d2bbc651dd5441b87e2
isereb <ruxxay.coder@gmail.com>
2018-10-25 16:59:23 -0400

    Finilized all attributes and added constructor

--- src/main/java/ru/anarok/audit/domain/AuditEvent.java
+++ src/main/java/ru/anarok/audit/domain/AuditEvent.java
@@ -1,16 +1,24 @@
 package ru.anarok.audit.domain;
 
-import lombok.Data;
+import lombok.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@Data
+@ToString
+@EqualsAndHashCode
+@Getter
 public class AuditEvent {
-    private long id;
-    private long timestamp = System.currentTimeMillis();
-    private String principal;
-    private String type;
-    private String message;
-    private Map<String, String> data = new HashMap<>();
+    private final long timestamp = System.currentTimeMillis();
+    private final String emitter;
+    private final String type;
+    private final String message;
+    @Setter(AccessLevel.NONE)
+    private final Map<String, String> data = new HashMap<>();
+
+    public AuditEvent(String emitter, String type, String message) {
+        this.emitter = emitter;
+        this.type = type;
+        this.message = message;
+    }
 }
